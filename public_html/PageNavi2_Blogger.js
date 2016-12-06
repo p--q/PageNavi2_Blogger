1bb50d9a4ace3d51da4e372dabf6358d2b64ac7d
pq <pq.blogspot@gmail.com>
2016-12-06 14:35:52 +0900

    event bubling

--- public_html/PageNavi2_Blogger.js
+++ public_html/PageNavi2_Blogger.js
@@ -1,6 +1,6 @@
-// PageNavi_Blogger2モジュール
-var PageNavi_Blogger2 = PageNavi_Blogger2 || function() {
-    var pg = {  // グローバルスコープに出すオブジェクト。グローバルスコープから呼び出すときはPageNavi_Blogger2になる。
+// PageNavi2_Bloggerモジュール
+var PageNavi2_Blogger = PageNavi2_Blogger || function() {
+    var pg = {  // グローバルスコープに出すオブジェクト。グローバルスコープから呼び出すときはPageNavi2_Bloggerになる。
         defaults : {  // 既定値。
             "perPage" : 10, //1ページあたりの投稿数。
             "numPages" : 5,  // ページナビに表示する通常ページボタンの数。スタートページからエンドページまで。
@@ -17,7 +17,7 @@ var PageNavi_Blogger2 = PageNavi_Blogger2 || function() {
             }, 
             getTotalPostCount : function(root){ 
                 var total_posts = parseInt(root.feed.openSearch$totalResults.$t, 10);  // 取得したフィードから総投稿総数を得る。
-                createButtons(total_posts);  // 総投稿数をもとにページナビのボタンを作成する。
+                createNodes(total_posts);  // 総投稿数をもとにページナビのボタンを作成する。
             }
         },
         all: function(array_elementIDs) {  // ここから開始する。引数にページナビを置換する要素のidを配列に入れる。
@@ -28,7 +28,7 @@ var PageNavi_Blogger2 = PageNavi_Blogger2 || function() {
             if (vars.elements.length > 0) {createPageNavi();}  // ページナビの作成。
         }
     }; // end of pg
-    var vars = {  // PageNavi_Blogger2モジュール内の"グローバル"変数。
+    var vars = {  // PageNavi2_Bloggerモジュール内の"グローバル"変数。
         perPage : pg.defaults.perPage,  // デフォルト値の取得。
         numPages : pg.defaults.numPages,  // デフォルト値の取得。
         window_width : pg.defaults.window_width,  // デフォルト値の取得。
@@ -38,7 +38,6 @@ var PageNavi_Blogger2 = PageNavi_Blogger2 || function() {
         currentPageNo : null,  // 現在のページ番号。
         elements : [],  // ページナビを挿入するhtmlの要素の配列。
         buttunElems : [],  // ボタン要素を入れる配列。
-        buttunFun : []  // ボタンのonclick属性を収納する配列。
     };
     function redirect(pageNo) {  // ページ番号のボタンをクリックされた時に呼び出される関数。
         vars.pageNo = pageNo;
@@ -46,16 +45,16 @@ var PageNavi_Blogger2 = PageNavi_Blogger2 || function() {
         var startPost = (vars.pageNo - 1) * vars.perPage;  // 新たに表示する先頭ページの先頭になる投稿番号を取得。
         var url;
         if (vars.postLabel) { 
-            url = "/feeds/posts/summary/-/" + vars.postLabel + "?start-index=" + startPost + "&max-results=1&alt=json-in-script&callback=PageNavi_Blogger2.callback.getURL";
+            url = "/feeds/posts/summary/-/" + vars.postLabel + "?start-index=" + startPost + "&max-results=1&alt=json-in-script&callback=PageNavi2_Blogger.callback.getURL";
         } else {
-            url = "/feeds/posts/summary?start-index=" + startPost + "&max-results=1&alt=json-in-script&callback=PageNavi_Blogger2.callback.getURL";
+            url = "/feeds/posts/summary?start-index=" + startPost + "&max-results=1&alt=json-in-script&callback=PageNavi2_Blogger.callback.getURL";
         }
         writeScript(url);
     }
     function createElem(tag){  // tagの要素を作成して返す。
        return document.createElement(tag); 
     }   
-    function createButtons(total_posts) {  // 総投稿数からページナビのボタンを作成。
+    function createNodes(total_posts) {  // 総投稿数からページナビのボタンを作成。
         var numPages = vars.numPages;  // ページナビに表示するページ数。
         var prevText = '«';  // 左向きスキップのための矢印。
         var nextText = '»';  // 右向きスキップのための矢印。
@@ -65,20 +64,20 @@ var PageNavi_Blogger2 = PageNavi_Blogger2 || function() {
         var lastPageNo = Math.ceil(total_posts / vars.perPage); // 総投稿数から総ページ数を算出。
         var pageEnd = pageStart + numPages - 1;  // エンドページの算出。
         if (pageEnd > lastPageNo) {pageEnd = lastPageNo;} // エンドページが総ページ数より大きい時はエンドページを総ページ数にする。
-        if (pageStart > 1) {vars.buttunElems.push(createFirstPageButton(1));}  // スタートページが2以上のときはスタートページより左に1ページ目のボタンを作成する。
-        if (pageStart == 3) {vars.buttunElems.push(createNormalButton(2, 2));} // スタートページが3のときはジャンプボタンの代わりに2ページ目のボタンを作成する。
+        if (pageStart > 1) {vars.buttunElems.push(createButton(1,1));}  // スタートページが2以上のときはスタートページより左に1ページ目のボタンを作成する。
+        if (pageStart == 3) {vars.buttunElems.push(createButton(2, 2));} // スタートページが3のときはジャンプボタンの代わりに2ページ目のボタンを作成する。
         if (pageStart > 3) {  // スタートページが4以上のときはジャンプボタンを作成する。
             var prevNumber = pageStart - vars.jumpPages + diff;  // ジャンプボタンでジャンプしたときに表示するページ番号。
-            (prevNumber < 2)?vars.buttunElems.push(createFirstPageButton(prevText)):vars.buttunElems.push(createNormalButton(prevNumber, prevText));  // ページ番号が1のときだけボタンの作り方が異なるための場合分け。
+            (prevNumber < 2)?vars.buttunElems.push(createButton(1,prevText)):vars.buttunElems.push(createButton(prevNumber, prevText));  // ページ番号が1のときだけボタンの作り方が異なるための場合分け。
         }
-        for (var j = pageStart; j <= pageEnd; j++) {vars.buttunElems.push((j == vars.currentPageNo)?createCurrentNode(j):((j == 1)?createFirstPageButton(j):createNormalButton(j, j)));}  // スタートボタンからエンドボタンまで作成。
-        if (pageEnd == lastPageNo - 2) {vars.buttunElems.push(createNormalButton(lastPageNo - 1, lastPageNo - 1));}  // エンドページと総ページ数の間に1ページしかないときは右ジャンプボタンは作成しない。
+        for (var j = pageStart; j <= pageEnd; j++) {vars.buttunElems.push((j == vars.currentPageNo)?createCurrentNode(j):createButton(j, j));}  // スタートボタンからエンドボタンまで作成。
+        if (pageEnd == lastPageNo - 2) {vars.buttunElems.push(createButton(lastPageNo - 1, lastPageNo - 1));}  // エンドページと総ページ数の間に1ページしかないときは右ジャンプボタンは作成しない。
         if (pageEnd < (lastPageNo - 2)) {  // エンドページが総ページ数より3小さい時だけ右ジャンプボタンを作成。
             var nextnumber = pageEnd + 1 + diff;  // ジャンプボタンでジャンプしたときに表示するページ番号。
             if (nextnumber > lastPageNo) {nextnumber = lastPageNo;} // 表示するページ番号が総ページ数になるときは総ページ数の番号にする。
-            vars.buttunElems.push(createNormalButton(nextnumber, nextText));  // 右ジャンプボタンの作成。
+            vars.buttunElems.push(createButton(nextnumber, nextText));  // 右ジャンプボタンの作成。
         }
-        if (pageEnd < lastPageNo) {vars.buttunElems.push(createNormalButton(lastPageNo, lastPageNo));}  // 総ページ番号ボタンの作成。
+        if (pageEnd < lastPageNo) {vars.buttunElems.push(createButton(lastPageNo, lastPageNo));}  // 総ページ番号ボタンの作成。
         writeHtml(pageStart, pageEnd, lastPageNo);  // htmlの書き込み。
     };
     function createPageNavi() {  // URLからラベル名と現在のページ番号を得、その後総投稿数を得るためのフィードを取得する。
@@ -90,35 +89,25 @@ var PageNavi_Blogger2 = PageNavi_Blogger2 || function() {
             vars.currentPageNo = (/#PageNo=/i.test(thisUrl))?/#PageNo=(\d+)/i.exec(thisUrl)[1]:1;  // URLから現在のページ番号の取得。
             var url;  // フィードを取得するためのURL。
             if (vars.postLabel) {  // 総投稿数取得のためにフィードを取得するURLの作成。ラベルインデックスのときはそのラベル名の総投稿数を取得するため。
-                url = '/feeds/posts/summary/-/' + vars.postLabel + "?alt=json-in-script&callback=PageNavi_Blogger2.callback.getTotalPostCount&max-results=1";          
+                url = '/feeds/posts/summary/-/' + vars.postLabel + "?alt=json-in-script&callback=PageNavi2_Blogger.callback.getTotalPostCount&max-results=1";          
             } else {
-                url = "/feeds/posts/summary?max-results=1&alt=json-in-script&callback=PageNavi_Blogger2.callback.getTotalPostCount";
+                url = "/feeds/posts/summary?max-results=1&alt=json-in-script&callback=PageNavi2_Blogger.callback.getTotalPostCount";
             }
             writeScript(url); 
         }
     };    
-    function createNormalButton(pageNo, text) {  // redirectするボタンの作成。
-        //ボタンから起動する関数はグローバルスコープから呼ばれるのでpgではなくPageNavi_Blogger2で呼び出す。
-        var spanNode = createButtonNode(text);      
-        spanNode.firstChild.href = "#";
-        spanNode.firstChild.name = pageNo;
-        //spanNode.firstChild.onclick = function() {
-//        vas.buttonFun.push(function() {
-//            redirect(pageNo,vars.perPage,vars.postLabel);
-//            return false;
-//        }); // vars.postLabelは文字列なので、クオーテーションが必要。undefinedも文字列として解釈される。
-        return spanNode;
-    }
-    function createFirstPageButton(text) {  // 1メージ目のボタンのhtmlを返す。ボタン表示は1かジャンプ矢印になる。
-        var spanNode = createButtonNode(text); 
-        spanNode.firstChild.href = (!vars.postLabel)?"/":"/search/label/" + vars.postLabel + "?max-results=" + vars.perPage;
-        return spanNode;
-    }
-    function createButtonNode(text){
+    function createButton(pageNo, text) {  // redirectするボタンの作成。
+        //ボタンから起動する関数はグローバルスコープから呼ばれるのでpgではなくPageNavi2_Bloggerで呼び出す。
         var spanNode = createElem('span');
         spanNode.className = "displaypageNum";
         spanNode.appendChild(createElem('a'));
         spanNode.firstChild.textContent = text;
+        if(pageNo==1) { // 1メージ目のボタンのNodeを返す。ボタン表示は1かジャンプ矢印になる。
+            spanNode.firstChild.href = (!vars.postLabel)?"/":"/search/label/" + vars.postLabel + "?max-results=" + vars.perPage;
+        } else {  // 1ページ目以外のボタンの作成。
+            spanNode.firstChild.href = "#";
+            spanNode.firstChild.name = pageNo;  // redirect()の引数に使う。
+        }
         return spanNode;
     }
     function createCurrentNode(j) {
@@ -133,103 +122,32 @@ var PageNavi_Blogger2 = PageNavi_Blogger2 || function() {
         ws.src = url;
         document.getElementsByTagName('head')[0].appendChild(ws);
     };
+    
+    
+    
     function writeHtml(pageStart, pageEnd, lastPageNo) {  // htmlの書き込み。
-        var dupNode;
-        var c;
         var divNode = createElem('div');
         vars.buttunElems.forEach(function(b){divNode.appendChild(b);});
-        vars.elements.forEach(function(e){
+        var dupNode;
+        vars.elements.forEach(function(elem){
             dupNode = divNode.cloneNode(true);
-            dupNode.childNodes.forEach(function(n) {
-                c = n.firstChild;
-                if(c.name) {
-                    c.onclick = Object.create(function() {
-                        redirect(c.name);
-                        return false;
-                    });
+            dupNode.onclick = function(e) {
+                e=e||event; // IE sucks
+                var target = e.target||e.srcElement; // targetはaになる。// and sucks again
+                // target is the element that has been clicked
+                if (target && target.parentNode.className=="displaypageNum") {
+                    redirect(target.name);
+                    return false; // stop event from bubbling elsewhere
                 }
-            });
-            e.appendChild(dupNode);
-            
-            
-//            vars.buttunElems.forEach(function(b){divNode.appendChild(b);});
-            
-            
-//            var dupNode = createElem('div');
-//            dupNode.appendChild(Object.create(divNode));
-//            e.appendChild(dupNode);
-             //var dupNode = divNode.cloneNode(true);
-//             var dupNode = divNode;
-            //dupNode.childelements.forEach(vas.buttonFun.forEach(function(b){.firstChild.onclick = );
-            
-            
-            
-//            e.appendChild(dupNode);
-//            e.textContent = null;
-//            var divNode = createElem('div');
-//            vars.buttunElems.forEach(function(b){divNode.appendChild(b);});
-//            e.appendChild(divNode);
+            };
+        elem.appendChild(dupNode);
         });  // 要素を書き換え。
-        //
-        //
-        //
-        //
-        //
-        //
-//        vars.elements[0].appendChild(divNode);
-        
-        
-        
-//        var left = vars.buttunElems.join('');  // 左ジャンプボタンとその左側のボタンのhtml。
-//        var center = vars.buttunElems.join('');  // スタートページからエンドページまでのボタンのhtml。
-//        var right = vars.buttunElems.join('');  // 右ジャンプボタンとその右側のボタンのhtml。
-//        var html;
-//        if (  window.innerWidth > vars.window_width || (pageEnd < 11) || (pageStart > lastPageNo - vars.numPages)) {  // すべてのボタンを1行で表示する場合。
-//            html = left + center + right; // 配列の要素をすべて結合する。
-//        } else {  // スクリーン幅がせまく、かつ、（表示の終わりのページ番号が10より大きい時、または、表示の始まりのページ番号が最終ページ番号とページナビに表示するページ数の差以下の時)
-//            if (pageEnd >= lastPageNo - 2) {  // 右のジャンプボタンがないときは右のみ次行に表示させない。
-//                center += right;
-//                right = null;
-//            }
-//            html = '<div style="text-align: center;line-height:2.2;">' + center + '</div>';
-//            if (left || right){  // ジャンプボタンより外側は2行目に表示する。
-//                html += '<div style="line-height:2.4;">';
-//                if (left) {html += '<span style="text-align: left;float: left;">' + left + '</span>';}
-//                if (right) {html += '<span style="float: right;">' + right + '</span>';}
-//                html += '</div>';
-//            } 
-//        }
-//        vars.elements.forEach(function(e){e.innerHTML = html;});  // 要素を書き換え。
     };
     return pg;
 }();
 //デフォルト値を変更したいときは以下のコメントアウトをはずして設定する。
-//PageNavi_Blogger2.defaults["perPage"] = 10 //1ページあたりの投稿数。
-//PageNavi_Blogger2.defaults["numPages"] = 5 // ページナビに表示するページ数。
-//PageNavi_Blogger2.defaults["window_width"] = 320 // ウィンドウ幅がこの幅px以下の時はページナビを2行にする。
-PageNavi_Blogger2.all(["blog-pager","blog-pager2"]);  // ページナビの起動。引き数にHTMLの要素のidを配列で入れる。
-//PageNavi_Blogger2.all(["blog-pager"]);  // ページナビの起動。引き数にHTMLの要素のidを配列で入れる。
-
-
-
-
-
-formObject.onclick = function(e)
-{
-    e=e||event; // IE sucks
-    var target = e.target||e.srcElement; // and sucks again
-
-    // target is the element that has been clicked
-    if (target && target.className=='remove') 
-    {
-        target.parentNode.parentNode.removeChild(target.parentNode);
-        return false; // stop event from bubbling elsewhere
-    }
-}
-
-+
-
-<div>
-  <input…>
-  <button type=button class=remove>Remove without JS handler!</button>
-</div>
+//PageNavi2_Blogger.defaults["perPage"] = 10 //1ページあたりの投稿数。
+//PageNavi2_Blogger.defaults["numPages"] = 5 // ページナビに表示するページ数。
+//PageNavi2_Blogger.defaults["window_width"] = 320 // ウィンドウ幅がこの幅px以下の時はページナビを2行にする。
+PageNavi2_Blogger.all(["blog-pager","blog-pager2"]);  // ページナビの起動。引き数にHTMLの要素のidを配列で入れる。
+//PageNavi2_Blogger.all(["blog-pager"]);  // ページナビの起動。引き数にHTMLの要素のidを配列で入れる。
